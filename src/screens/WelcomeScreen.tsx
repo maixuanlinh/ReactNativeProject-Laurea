7412c446bf97be29c9809ea4f6cf6023cf2a6408
maixuanlinh <maixuanlinhdkh@gmail.com>
2023-11-20 00:53:30 +0200

    add Welcome Screen

---
+++ src/screens/WelcomeScreen.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../../App';
+
+type WelcomeScreenNavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  'Welcome'
+>;
+
+type Props = {
+  navigation: WelcomeScreenNavigationProp;
+};
+
+const WelcomeScreen: React.FC<Props> = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to the To-Do List App</Text>
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Go to The To-Do List App"
+          onPress={() => navigation.navigate('SignIn')}
+        />
+      </View>
+
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 22,
+    marginBottom: 20,
+  },
+  buttonContainer: {
+    marginBottom: 10, 
+  },
+});
+
+export default WelcomeScreen;
